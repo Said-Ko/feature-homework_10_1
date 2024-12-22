84eb5ed81758c14742f8dcb8bfa48368de0773de
Саид <said_ccc@mail.ru>
2024-12-22 23:11:58 +0300

    README add info fixed annotation for second func change name sort_, dict_ to dict_of_list, revers_date_list

--- src/processing.py
+++ src/processing.py
@@ -10,7 +10,7 @@ def filter_by_state(tested_list: List[Dict], value_: str = 'EXECUTED') -> List[D
     return executed_list
 
 
-def sort_by_date(tested_list: List[Dict], revers_date_list: bool = True) -> List[Dict]:
+def sort_by_date(tested_list: List[Dict], `revers_date_list`: bool = True) -> List[Dict]:
     """Функция сортировки словарей в списке по убыванию даты"""
     sorted_list_by_date = sorted(tested_list, key=lambda x: x.get('date'), reverse=revers_date_list)
     return sorted_list_by_date
