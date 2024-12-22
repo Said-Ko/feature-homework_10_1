e74c7d6161415659eee7bce590a5a27beb08cb9b
Саид <said_ccc@mail.ru>
2024-12-22 19:37:31 +0300

    README add info fixed annotation for second func change name sort_, dict_ to dict_of_list, revers_date_list

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from src.processing import filter_by_state, sort_by_date
+from processing import filter_by_state, sort_by_date
 
 tested_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
                {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
--- src/processing.py
+++ src/processing.py
@@ -2,15 +2,17 @@ from typing import Dict, List
 
 
 def filter_by_state(tested_list: List[Dict], value_: str = 'EXECUTED') -> List[Dict]:
-    """Фунция сбора списка со статусом 'EXECUTED' """
+    """Фунция принимающая на входе список словарей, и выдает спиосок словарей со статусом 'EXECUTED' """
     executed_list = []
-    for dict_ in tested_list:
-        if dict_['state'] == 'EXECUTED':
-            executed_list.append(dict_)
+    for dict_of_list in tested_list:
+        if dict_of_list['state'] == 'EXECUTED':
+            executed_list.append(dict_of_list)
     return executed_list
 
 
-def sort_by_date(tested_list: List[Dict], sort_: bool = True) -> List[Dict]:
+def sort_by_date(tested_list: List[Dict], revers_date_list: bool = True) -> List[Dict]:
     """Функция сортировки словарей в списке по убыванию даты"""
-    sorted_list_by_date = sorted(tested_list, key=lambda x: x.get('date'), reverse=sort_)
+    sorted_list_by_date = sorted(tested_list, key=lambda x: x.get('date'), reverse=revers_date_list)
     return sorted_list_by_date
+
+
